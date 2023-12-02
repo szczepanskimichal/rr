3e53d6238834136123a6e5ed9bb13da7ccbfeae1
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-02 21:26:55 +0100

    form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
-class Form extends Comment {
+class Form extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    console.log(form.elements);
+    const inputVal = form.elements[0].value;
+    // console.log(event.currentTarget);
+    console.log("submit", inputVal);
+  }
+
   render() {
-    return <>Form</>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" />
+        <button type="submit">Submit</button>
+      </form>
+    );
   }
 }
 
