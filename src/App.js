ca8e565a6f8d198c824f5b2171fd4e1da377c19e
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-02 08:11:14 +0100

    working
    developing and repeating React topics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React from "react";
 import Header from "./components/Header";
 
-function App() {
-  const isLogged = true;
+//------------------------------------------------------------
+// && operator
+// function App() {
+//   //   const isLogged = false;
+//   const tab = [];
+//   return (
+//     <>
+//       <p>{tab.length && <p>los</p>}</p>
+//     </>
+//   );
+// }
 
-  if (!isLogged) {
-    return <div>Logg in</div>;
-  }
+//------------------------------------------------------------
+// TERNARY OPERATOR
+// function App() {
+//   const isLogged = true;
 
-  //   return <div>Logged in</div>;
-  return <Header />;
-}
+//   return (
+//     <>
+//       <p>
+//         {isLogged ? (
+//           <>
+//             Logged in
+//             <span>Cos</span>
+//           </>
+//         ) : (
+//           <a>Logg in</a>
+//         )}
+//       </p>
+//     </>
+//   );
+// }
+//------------------------------------------------------------
+// IF ELSE
+// function App() {
+//   const isLogged = true;
+
+//   if (!isLogged) {
+//     return <div>Logg in</div>;
+//   }
+
+//   //   return <div>Logged in</div>;
+//   return <Header />;
+// }
 
 export default App;
