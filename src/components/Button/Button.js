6bb6a59f9275a65ebdeae1be6f9e4aedb32fc6b9
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-02 08:51:42 +0100

    working
    styling

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,28 @@
+// export default function Button() {
+//   return (
+//     <div
+//       style={{
+//         color: "red",
+//         backgroundColor: "blue",
+//         margin: 10,
+//         fontSize: 20,
+//         border: "3px solid green",
+//       }}
+//     >
+//       Button
+//     </div>
+//   );
+// }
+
+// Destructuring
+const style = {
+  color: "red",
+  backgroundColor: "blue",
+  margin: 10,
+  fontSize: 20,
+  border: "3px solid green",
+};
+
 export default function Button() {
-  return (
-    <div
-      style={{
-        color: "red",
-        backgroundColor: "blue",
-        margin: 10,
-      }}
-    >
-      Button
-    </div>
-  );
+  return <div style={style}>Button</div>;
 }
