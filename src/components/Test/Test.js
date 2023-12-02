66bb91e928dd45ef6a6de36460994243453191b3
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-02 20:20:32 +0100

    message from child to parent

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -24,60 +24,99 @@ import React, { Component } from "react";
 //   }
 // }
 
-class Test extends Component {
+// class Test extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       counter: this.props.x,
+//       name: "Mija",
+//       tab: [1, 2, 3],
+//     };
+//   }
+//   increment = () => {
+//     // console.log("increment", this.state.counter);
+//     // this.setState({ counter: 999 });
+//     // console.log(this);
+//     // const { counter, name } = this.state;
+//     // console.log("increment", this.state.counter);
+
+//     this.setState((prevState, props) => ({
+//       counter: prevState.counter + 1,
+//       name: "Ada",
+//     }));
+
+//     // this.setState((prev) => {
+//     //   return {
+//     //     tab: [...prev.tab, 4],
+//     //   };
+//     // });
+//     console.log("increment", this.state.counter);
+
+//     this.setState(
+//       {
+//         name: "Ada",
+//       },
+//       () => {
+//         console.log("after state update", this.state.name);
+//       }
+//     );
+//   };
+
+// render() {
+//     console.log("render");
+//     const { counter, name, tab } = this.state;
+//     return (
+//       <>
+//         <button onClick={this.increment}>
+//           CLICK - {counter}
+//           <p>{name}</p>
+//         </button>
+//         <h1>{tab}</h1>
+//       </>
+//     );
+//   }
+// }
+//------------------------------------------------------------
+
+function Child({ user, foo }) {
+  const handleClick = () => {
+    foo("Message from child to parent", user);
+  };
+  return (
+    <div>
+      {user} - <button onClick={handleClick}>REMOVE</button>
+    </div>
+  );
+}
+
+class Parent extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      counter: this.props.x,
-      name: "Mija",
-      tab: [1, 2, 3],
+      list: ["Mija", "Ada", "Marysia"],
     };
   }
-  increment = () => {
-    // console.log("increment", this.state.counter);
-    // this.setState({ counter: 999 });
-    // console.log(this);
-    // const { counter, name } = this.state;
-    // console.log("increment", this.state.counter);
 
-    this.setState((prevState, props) => ({
-      counter: prevState.counter + 1,
-      name: "Ada",
-    }));
-
-    // this.setState((prev) => {
-    //   return {
-    //     tab: [...prev.tab, 4],
-    //   };
-    // });
-    console.log("increment", this.state.counter);
-
-    this.setState(
-      {
-        name: "Ada",
-      },
-      () => {
-        console.log("after state update", this.state.name);
-      }
-    );
+  send = (msg) => {
+    console.log(msg);
   };
+
   render() {
-    console.log("render");
-    const { counter, name, tab } = this.state;
+    console.log("render parent");
+    const { list } = this.state;
     return (
       <>
-        <button onClick={this.increment}>
-          CLICK - {counter}
-          <p>{name}</p>
-        </button>
-        <h1>{tab}</h1>
+        {list.map((user) => (
+          <Child key={user} user={user} foo={this.send} />
+        ))}
       </>
     );
   }
 }
 
-export default Test;
+export default Parent;
 
 //   foo() {
 //     console.log("foo");
