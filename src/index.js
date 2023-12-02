943890c09e85f225c9cd1ebf5fd7ed3d0f1875ab
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-02 07:37:33 +0100

    working

--- src/index.js
+++ src/index.js
@@ -1,45 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.css";
+import "./index.css";
+import PropTypes from "prop-types";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Post(props) {
-  return (
-    <div>
-      {props.name} - {props.surname}
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div>
-      <Post name="Jan" surname="Kowalski" />
-    </div>
-  );
-}
-
 root.render(<App />);
-
-// ---------------------------------------------------------------------------------------
-//   //reactFragment
-// const name = "Bob";
-
-// function generatedId(id) {
-//   return "moje" + id;
-// }
-
-// const text = <p>cos2</p>;
-// const user = { name: "Ala" };
-
-// root.render(
-//   //reactFragment
-//   <>
-//     <h1 id={generatedId("cos")}>
-//       {text} Name: {name.toUpperCase()}
-//       <soab>{user.name}</soab>
-//     </h1>
-//     <p>Cos nowego</p>
-//   </>
-// );
