7a470148bdc5dc204185d9c9c982125e9e51493c
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-03 09:12:56 +0100

    controlled forms

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,21 +1,90 @@
 import React, { Component } from "react";
 
+const initValues = {
+  username: "Welcome",
+  password: "",
+  isChecked: false,
+  color: null,
+};
+
 class Form extends Component {
-  handleSubmit(event) {
+  // xyz = 0;
+  state = {
+    ...initValues,
+  };
+
+  handleSubmit = (event) => {
     event.preventDefault();
-    const form = event.currentTarget;
-    console.log(form.elements);
-    const inputVal = form.elements[0].value;
-    // console.log(event.currentTarget);
-    console.log("submit", inputVal);
-  }
+    console.log(this.state.username);
+    // this.setState(initValues);
+    this.reset();
+    // this.setState({
+    //   username: "",
+    //   password: "",
+    // });
+  };
+  //   // const form = event.currentTarget;
+  //   // console.log(form.elements);
+  //   // const inputVal = form.elements.username.value;
+  //   // // console.log(event.currentTarget);
+  //   // console.log("submit", inputVal);
+  // }
+
+  handleChange = (event) => {
+    // console.log(event.target.value);
+    const { name, value, type, checked } = event.target;
+    this.setState({
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  reset = () => {
+    this.setState(initValues);
+  };
+
+  // handleChangeChecbox = (event) => {
+  //   console.log(event.target.checked);
+  //   this.setState({
+  //     isChecked: event.target.checked,
+  //   });
+  // };
 
   render() {
+    console.log(this);
+    const { username, password, isChecked } = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" />
-        <button type="submit">Submit</button>
-      </form>
+      <>
+        <h1>{username}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          <input
+            type="checkbox"
+            name="isChecked"
+            checked={isChecked}
+            onChange={this.handleChange}
+          />
+          <label>
+            BLACK
+            <input type="radio" />
+          </label>
+          <label>
+            WHITE
+            <input type="radio" />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </>
     );
   }
 }
