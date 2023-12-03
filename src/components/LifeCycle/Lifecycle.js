c7666959036a026f384425bf47692dcfce032e3c
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-03 12:05:07 +0100

    add folder lifecycle

---
+++ src/components/LifeCycle/Lifecycle.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Child extends Component {
+  render() {
+    return <div>Child</div>;
+  }
+}
+class Livecycle extends Component {
+  render() {
+    return <>LIFE CYCLE</>;
+  }
+}
+
+export default Livecycle;
