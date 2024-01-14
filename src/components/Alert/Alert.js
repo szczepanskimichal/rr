64ff364ecbf067b3775003a30fc319e350f5abca
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-01-14 13:12:19 +0100

    commit

--- src/components/Alert/Alert.js
+++
@@ -1,5 +0,0 @@
-import css from "./alert.module.css";
-
-export default function Alert() {
-  return <div className={css.button}>Alert</div>;
-}
--- src/components/Alert/Button.js
+++
@@ -1,3 +0,0 @@
-export default function Button2() {
-  return <div>Button2</div>;
-}
